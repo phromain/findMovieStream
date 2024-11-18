b23951ae9cf311b13fbe539a506358b11efe0218
phromain <phromain@hotmail.fr>
2024-11-18 15:06:00 +0100

    chore: configuration de a structure du projet et création des composants initiaux

---
+++ src/app/components/movie-list-grid/movie-list-grid.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-movie-list-grid',
+  standalone: true,
+  imports: [],
+  templateUrl: './movie-list-grid.component.html',
+  styleUrl: './movie-list-grid.component.css'
+})
+export class MovieListGridComponent {
+
+}
