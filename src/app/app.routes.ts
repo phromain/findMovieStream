ef17e5d6111af06dbf00fc3b77cf02a6f8ec8138
phromain <phromain@hotmail.fr>
2024-11-19 17:33:44 +0100

    feat: Ajout des routes + Creéation page 404

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,13 @@
 import { Routes } from '@angular/router';
 import { MovieSearchComponent } from './page/movie-search/movie-search.component';
+import { MovieListComponent } from './page/movie-list/movie-list.component';
+import { MovieDetailComponent } from './page/movie-detail/movie-detail.component';
+import { PageNotFoundComponent } from './page/page-not-found/page-not-found.component';
 
 export const routes: Routes = [
     { path: '', component: MovieSearchComponent },
+    { path: 'liste', component: MovieListComponent },
+    { path: 'detail', component: MovieDetailComponent },
+    { path: '**', component: PageNotFoundComponent },
+
 ];
