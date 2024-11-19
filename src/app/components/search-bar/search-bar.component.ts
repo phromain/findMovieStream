2e5f24187ddd16f89295bdb237573ca49a32dabc
phromain <phromain@hotmail.fr>
2024-11-19 17:16:39 +0100

    feat(search) : ajout de la méthode searchMovies dans MovieService et composant SearchBar

--- src/app/components/search-bar/search-bar.component.ts
+++ src/app/components/search-bar/search-bar.component.ts
@@ -1,12 +1,34 @@
 import { Component } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { MovieService } from '../../service/movie.service';
+import { Movie } from '../../model/movie';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-search-bar',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule, HttpClientModule],
   templateUrl: './search-bar.component.html',
-  styleUrl: './search-bar.component.css'
+  styleUrls: ['./search-bar.component.css'],
+  providers: [MovieService]
 })
 export class SearchBarComponent {
+  query: string = '';
+  movies: Movie[] = [];
 
+  constructor(private movieService: MovieService) { }
+
+  searchMovies(): void {
+    if (this.query) {
+      this.movieService.searchMovies(this.query).subscribe({
+        next: (data: Movie[]) => {
+          this.movies = data;
+        },
+        error: (err: any) => {
+          console.error(err);
+        }
+      });
+    }
+  }
 }
--- src/app/service/movie.service.ts
+++ src/app/service/movie.service.ts
@@ -1,9 +1,50 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { catchError, map } from 'rxjs/operators';
+import { Movie } from '../model/movie';
+import { Provider } from '../model/provider';
+import { ENDPOINTS, BASE_URL_IMG, ERROR_MESSAGES, API_KEY } from '../const/api';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MovieService {
+  private selectedMovieId: number | null = null;
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  setSelectedMovieId(movieId: number): void {
+    this.selectedMovieId = movieId;
+  }
+
+  getSelectedMovieId(): number | null {
+    return this.selectedMovieId;
+  }
+
+  searchMovies(query: string): Observable<Movie[]> {
+    const url = `${ENDPOINTS.movie.search}?api_key=${API_KEY}&query=${encodeURIComponent(query)}&language=fr`;
+    return this.http.get<{ results: any[] }>(url).pipe(
+      map(response => {
+        if (!response.results) {
+          throw new Error(ERROR_MESSAGES.noResults);
+        }
+        return response.results.map(item => new Movie(
+          item.id,
+          item.title,
+          item.poster_path ? `${BASE_URL_IMG}${item.poster_path}` : null,
+          item.overview || 'Pas de résumé disponible',
+          item.release_date || 'Date non spécifiée'
+        ));
+      }),
+      catchError(error => throwError(this.handleError(error)))
+    );
+  }
+
+  private handleError(error: any): string {
+    if (error.status) {
+      return ERROR_MESSAGES.httpError(error.status);
+    }
+    return error.message ;
+  }
 }
