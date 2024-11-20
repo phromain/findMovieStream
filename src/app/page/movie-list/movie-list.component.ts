22e3424d3f7af1a3d9b232a53a8aa9573adba4c0
phromain <phromain@hotmail.fr>
2024-11-20 21:11:50 +0100

    feat: ajout curseur au survol de l'image

--- src/app/page/movie-list/movie-list.component.ts
+++ src/app/page/movie-list/movie-list.component.ts
@@ -1,11 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-import { MovieService } from '../../service/movie.service';
 import { CommonModule } from '@angular/common';
-import { Movie } from '../../model/movie';
 import { HeaderComponent } from "../../components/template/header/header.component";
 import { FooterComponent } from "../../components/template/footer/footer.component";
 import { HttpClientModule } from '@angular/common/http';
-import { Router, RouterModule } from '@angular/router';
+import { RouterModule } from '@angular/router';
 import { MovieListGridComponent } from "../../components/movie-list-grid/movie-list-grid.component";
 
 @Component({
